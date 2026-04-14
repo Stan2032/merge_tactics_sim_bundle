8e16b0206f0725f5dd1975fce22ae90544463e4a
S <68326386+Stan2032@users.noreply.github.com>
2026-04-14 01:04:30 +0100

    Add coordinates and cooldown to Unit initialization

--- engine/unit.py
+++ engine/unit.py
@@ -1,7 +1,22 @@
+from data.troops import TROOPS
+
 class Unit:
-    def __init__(self, name):
+    def __init__(self, name, x=0, y=0):
+        data = TROOPS[name]
+
         self.name = name
+        self.traits = data["traits"]
+        self.role = data["role"]
+
         self.hp = 100
         self.dps = 10
-        self.x = 0
-        self.y = 0
+        self.range = 1
+
+        self.x = x
+        self.y = y
+
+        self.cooldown = 0
+        self.locked_until = 0
+
+    def __repr__(self):
+        return f"{self.name}(HP={self.hp})"
