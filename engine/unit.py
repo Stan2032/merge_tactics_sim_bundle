eb1ee302e5d6428b6f7717ae6e7e3aeb43e08fee
S <68326386+Stan2032@users.noreply.github.com>
2026-04-14 01:20:32 +0100

    Implement distance method in Unit class
    Add distance method to calculate distance between units

--- engine/unit.py
+++ engine/unit.py
@@ -1,5 +1,6 @@
 from data.troops import TROOPS
 
+
 class Unit:
     def __init__(self, name, x=0, y=0):
         data = TROOPS[name]
@@ -18,5 +19,5 @@ class Unit:
         self.cooldown = 0
         self.locked_until = 0
 
-    def __repr__(self):
-        return f"{self.name}(HP={self.hp})"
+    def distance(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
