c0b14f5f6d0b60af4f505e05ba69737aace03ed0
S <68326386+Stan2032@users.noreply.github.com>
2026-04-14 01:02:39 +0100

    Add verified trait system

---
+++ data/traits.py
@@ -0,0 +1,16 @@
+# Verified 13 traits
+TRAITS = {
+    "Ace": {"thresholds": [2,4]},
+    "Assassin": {"thresholds": [2,4]},
+    "Blaster": {"thresholds": [2,4]},
+    "Brawler": {"thresholds": [2,4]},
+    "Brutalist": {"thresholds": [2,4]},
+    "Clan": {"thresholds": [2,4]},
+    "Giant": {"thresholds": [2]},
+    "Goblin": {"thresholds": [2,4]},
+    "PEKKA": {"thresholds": [2]},
+    "Noble": {"thresholds": [2,4]},
+    "Ranger": {"thresholds": [2,4]},
+    "Superstar": {"thresholds": [2,4]},
+    "Undead": {"thresholds": [2,4]}
+}
